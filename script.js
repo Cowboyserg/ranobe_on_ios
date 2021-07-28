a014edee6e3f432aeb6fbf28e52d8833132d2516
Cowboyserg <skuzmenkov@list.ru>
2021-07-29 01:21:18 +0300

    5

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const navInit = () => {
         alert("Двигаем");
+        document.addEventListener("touchmove", touchMove, false);
         const nav = document.querySelector('.nav'); // ищем блок навигации
         const links = document.querySelectorAll('.nav__li'); // ищем все навигационные ссылки
         const sections = document.querySelectorAll('.section'); // ищем все секции
@@ -9,8 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
         document.cookie="where="+window.pageYOffset;
         console.log("Запомнили новое положение", document.cookie.split(";")[1].split("=")[1]);
     }
+
+    function touchMove(event) {
+        alert("Двигаем на айфоне");
+    }
     navInit() // запускаем функцию при загрузке страницы
     window.addEventListener('scroll', () => {
+        // alert("Двигаем1");
         navInit(); // запускаем функцию при скролле страницы
     })
     // window.addEventListener('resize', () => {
