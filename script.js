49c241e115e1d46789f1dfaf4a855ed9c4119bdb
Cowboyserg <skuzmenkov@list.ru>
2021-07-29 00:11:04 +0300

    fix scroll to stop

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const navInit = () => {
-        const nav = document.querySelector('.nav') // ищем блок навигации
-        const links = document.querySelectorAll('.nav__li') // ищем все навигационные ссылки
-        const sections = document.querySelectorAll('.section') // ищем все секции
-        localStorage.setItem("where", document.cookie)
-        console.log(window.pageYOffset);
-        console.log(document.cookie);
+        const nav = document.querySelector('.nav'); // ищем блок навигации
+        const links = document.querySelectorAll('.nav__li'); // ищем все навигационные ссылки
+        const sections = document.querySelectorAll('.section'); // ищем все секции
+        // localStorage.setItem("where", window.pageYOffset);
+        document.cookie="where="+window.pageYOffset;
+        console.log("Запомнили новое положение", document.cookie.split(";")[1].split("=")[1]);
     }
     navInit() // запускаем функцию при загрузке страницы
     window.addEventListener('scroll', () => {
-        navInit() // запускаем функцию при скролле страницы
+        navInit(); // запускаем функцию при скролле страницы
     })
-    window.addEventListener('resize', () => {
-        navInit() // запускаем функцию при ресайзе страницы
-    })
+    // window.addEventListener('resize', () => {
+    //     navInit() // запускаем функцию при ресайзе страницы
+    // })
 })
 
 document.onreadystatechange = function () {
-    let pos = localStorage.getItem("where");
-    window.scrollTo(0, pos)
+    // let pos = localStorage.getItem("where");
+    let pos = document.cookie.split(";")[1].split("=")[1];
+    window.scrollTo(0, pos);
     console.log("Открутили", pos);
     // window.scrollTo(0, document.cookie);
 }
