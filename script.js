bbe727180bd0bfb9f3845f6f3ba0503da8738e21
Cowboyserg <skuzmenkov@list.ru>
2021-07-29 15:54:08 +0300

    remove cookie

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navInit = () => {
-        alert("Загрузили страницу"+document.cookie);
-        console.log("Загрузили страницу");
+        alert("Загрузили страницу:"+localStorage.getItem("where"));
+        console.log("Загрузили страницу:");
         document.addEventListener("touchmove", touchMove, false);
         const nav = document.querySelector('.nav'); // ищем блок навигации
         const links = document.querySelectorAll('.nav__li'); // ищем все навигационные ссылки
         const sections = document.querySelectorAll('.section'); // ищем все секции
         localStorage.setItem('where', window.pageYOffset.toString());
         // console.log("Запомнили новое положение", document.cookie.split(";")[1].split("=")[1]);
-        console.log("Запомнили новое положение", localStorage.getItem("where"));
+        console.log("Запомнили новое положение:", localStorage.getItem("where"));
     }
 
     window.addEventListener('scroll', () => {
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function touchMove(event) {
     // document.cookie="where="+window.pageYOffset;
     // document.cookie=window.pageYOffset;
-    localStorage.setItem('where', window.pageYOffset.toString());
+    localStorage.setItem('where:', window.pageYOffset.toString());
     // alert("Двигаем на айфоне"+localStorage.getItem("where"));
 }
 
